df37640eadd5c64649fbe6d54c68d73f50a05525
arturusfury <bri.reynolds@gmail.com>
2016-11-07 10:19:40 -0600

    figured out how to load data from user profile, tried loading posts with no luck yet

--- app/facebook/facebook.js
+++ app/facebook/facebook.js
@@ -7,12 +7,14 @@ angular.module('ngSocial.facebook', ['ngRoute', 'ngFacebook'])
     templateUrl: 'facebook/facebook.html',
     controller: 'FacebookCtrl'
   });
-  $facebookProvider.setAppId('1811866962360825');
-  $facebookProvider.setPermissions('email', 'public_profile', 'user_posts', 'publish_actions', 'user_photos');
 }])
 
+.config(function($facebookProvider) {
+  $facebookProvider.setAppId('1811866962360825');
+  $facebookProvider.setPermissions('email', 'public_profile', 'user_posts', 'publish_actions', 'user_photos');
+})
+
 .run(function($rootScope) {
-  // Cut and paste the "Load the SDK" code from the facebook javascript sdk page.
   (function(d, s, id){
        var js, fjs = d.getElementsByTagName(s)[0];
        if (d.getElementById(id)) {return;}
@@ -22,6 +24,45 @@ angular.module('ngSocial.facebook', ['ngRoute', 'ngFacebook'])
      }(document, 'script', 'facebook-jssdk'));
 })
 
-.controller('FacebookCtrl', [function() {
+.controller('FacebookCtrl', ['$scope', '$facebook', function($scope, $facebook) {
+  $scope.isLoggedIn = false;
 
+  $scope.login = function() {
+    $facebook.login().then(function() {
+      $scope.isLoggedIn = true;
+      refresh();
+    });
+  }
+
+  $scope.logout = function() {
+    $facebook.logout().then(function(response) {
+      $scope.isLoggedin = false;
+    });
+  }
+
+  function refresh() {
+    $facebook.api('/me?fields=name,id,first_name,last_name,email,gender,locale,link,posts').then(function(response) {
+      $scope.welcomeMsg = "Welcome " + response.name;
+      $scope.userInfo = response;
+
+      console.log($scope.userInfo);
+
+      $facebook.api('/me/picture').then(function (response) {
+        $scope.picture = response.data.url;
+      });
+
+      $facebook.api('/me/permissions').then(function(response) {
+        $scope.permissions = response.data;
+      });
+
+      $facebook.api('/me/posts').then(function(response) {
+        $scope.wallPosts = response.data;
+      });
+    },
+    function (err){
+      $scope.welcomeMsg = "Please Login to Facebook";
+    });
+  }
+
+  refresh();
 }]);
